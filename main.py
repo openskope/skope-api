8085452da4e10eddcb8225edd03bb602597afc36
Calvin Pritchard <pritchard.calvin@gmail.com>
2021-02-11 10:29:04 -0700

    feat: added geojson point handling
    still have to handle out of bounds indice data

--- main.py
+++ main.py
@@ -4,13 +4,25 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import rasterio
 import numpy as np
+import logging
 from rasterio.windows import Window
 from rasterio.mask import raster_geometry_mask
+from scipy import stats
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel('DEBUG')
 
 
 class ZonalStatistic(str, Enum):
     mean = 'mean'
     median = 'median'
+
+    def to_numpy_call(self):
+        if self == self.mean:
+            return np.mean
+        elif self == self.median:
+            return np.median
 
 
 class Geometry(BaseModel):
@@ -74,22 +86,33 @@ app = FastAPI()
 
 
 def extract_point(dataset: rasterio.DatasetReader, point: Point) -> np.array:
+    print(f'extracting point: {point.to_string()}')
     px, py = dataset.index(point.coordinates[0], point.coordinates[1])
-    return dataset.read(window=Window(px, py, 1, 1))
+    print(f'indices: ({px}, {py})')
+    return dataset.read(window=Window(px, py, 1, 1)).flatten()
 
 
-def extract_polygon(dataset: rasterio.DatasetReader, polygon: Polygon) -> np.array:
+def extract_polygon(dataset: rasterio.DatasetReader, zonal_statistics: ZonalStatistic, polygon: Polygon) -> np.array:
+    print(f'extracting polygon: {polygon.to_string()}')
+    zonal_func = zonal_statistics.to_numpy_call()
     masked, transform, window = raster_geometry_mask(dataset, [polygon], crop=True, all_touched=True)
     result = np.zeros(dataset.count, dtype=dataset.dtypes[0])
     for band in range(dataset.count):
         data = dataset.read(band + 1, window=window)
-        result[band] = np.mean(data)
+        values = np.ma.array(data=data, mask=np.logical_or(np.equal(data, dataset.nodata), masked))
+        result[band] = zonal_func(values)
+
     return result
 
 
 # add request timeout middleware https://github.com/tiangolo/fastapi/issues/1752
 @app.post("/datasets/{datasetId}", response_model=AnalysisResponse)
 def read_root(datasetId: str, data: AnalysisRequest):
-    with rasterio.open(f'data/{datasetId}.tif') as src:
-        w = src.read(window=Window(1,1,1,1)).flatten()
+    with rasterio.open(f'data/{datasetId}.tif') as dataset:
+        if data.selectedArea.type == 'Point':
+            w = extract_point(dataset=dataset, point=data.selectedArea)
+        else:
+            w = extract_polygon(dataset=dataset, zonal_statistics=data.zonalStatistic, polygon=data.selectedArea)
+    if data.coordinateTransform == CoordinateTransform.zscore:
+        w = stats.zscore(w)
     return AnalysisResponse(timeRange=(YearMonth(year=1500), YearMonth(year=1800)), values=list(w))
