fix: moving average test

app/tests/routers/test_datasets.py
@@ -9,8 +9,7 @@ client = TestClient(app)
 
 def test_moving_average_smoother():
     xs = np.array([1,1,1,1,1,2,2,2,2,2])
-    mas = ds.MovingAverageSmoother(method='centered', width=5)
+    mas = ds.MovingAverageSmoother(method='centered', width=2)
     smoothed_xs = mas.apply(xs)
-    assert True
-    # assert np.allclose(smoothed_xs, np.array([1, (4 + 2)/5, (3 + 4)/5, (2 + 6)/5, (1 + 8)/5, 2]))
+    assert np.allclose(smoothed_xs, np.array([1, (4 + 2)/5, (3 + 4)/5, (2 + 6)/5, (1 + 8)/5, 2]))
 