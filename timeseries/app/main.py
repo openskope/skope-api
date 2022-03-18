8bcc9dab91ab5375a2222d84a2255c39a03b7ad9
Allen Lee <allen.lee@asu.edu>
2022-03-17 20:26:43 -0700

    refactor: remove dead code, add black dep
    TODO: convert settings to pydantic BaseSettings

--- timeseries/app/main.py
+++ timeseries/app/main.py
@@ -9,16 +9,14 @@ from app.settings import settings
 from app.routers.v1 import api as v1_api
 from app.routers.v2 import api as v2_api
 
-settings.init()
 logger = logging.getLogger(__name__)
-
 
 app = FastAPI(
     title='OpenSKOPE Time Series API'
 )
 
 # Since the whole API is public there is no danger in allowing all cross origin requests for now
-app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_methods=['*'], allow_headers=['*'])
+app.add_middleware(CORSMiddleware, allow_origins=settings.ALLOWED_ORIGINS, allow_methods=['*'], allow_headers=['*'])
 
 
 @app.exception_handler(TimeseriesTimeoutError)
@@ -26,9 +24,9 @@ async def timeseries_timeout_error_handler(request: Request, exc: TimeseriesTime
     return JSONResponse(
         status_code=HTTP_504_GATEWAY_TIMEOUT,
         content={
-        'detail': exc.message,
-        'processing_time': exc.processing_time
-    })
+            'detail': exc.message,
+            'processing_time': exc.processing_time
+        })
 
 
 @app.exception_handler(TimeseriesValidationError)
