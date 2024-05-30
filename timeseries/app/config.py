27443ca9ea252e7d83dba5c7d0532cb31fe4ed1b
Allen Lee <alee@users.noreply.github.com>
2024-05-29 23:01:26 -0700

    fix: replace defunct sentry dsn

--- timeseries/app/config.py
+++ timeseries/app/config.py
@@ -32,7 +32,7 @@ class Settings(BaseSettings):
     max_processing_time = 15000  # in milliseconds
     default_max_cells = 500000  # max number of cells to extract from data cubes
     store: Store
-    sentry_dsn = "https://79be8c27ac14446e8af51b6d5f3dc90f@sentry.comses.net/5"
+    sentry_dsn = "https://9b9dc2f60562380edeb675c39fe1c896@sentry.comses.net/4"
 
     @classmethod
     def create(cls):
